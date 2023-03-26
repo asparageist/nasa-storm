d0b47dfe10fe4de1bdea2884b5937ac2c1ae70a1
Billy Lee <codingbillylee@gmail.com>
2023-03-26 09:33:07 -0700

    Initial commit

--- webpack.config.js
+++ webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const Dotenv = require('dotenv-webpack');
 const ESLintPlugin = require('eslint-webpack-plugin');
 module.exports = {
   entry: './src/index.js',
@@ -14,6 +15,7 @@ module.exports = {
   devtool: 'eval-source-map',
   plugins: [
     new ESLintPlugin(),
+    new Dotenv(),
     new CleanWebpackPlugin({
       verbose: true
     }), 
