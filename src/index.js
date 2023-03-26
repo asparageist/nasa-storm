7ba0328f562c38f656dd744aae853a33e7729b44
Billy Lee <codingbillylee@gmail.com>
2023-03-26 15:32:27 -0700

    Add initial content to landing page

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import pictureOTD from './api/potdApiCall.js';
-
+// import * as THREE from 'three';
 
 pictureOTD.getPicture()
   .then((data) => {
@@ -12,7 +12,16 @@ pictureOTD.getPicture()
       errorElement.innerHTML = errorMessage;
       throw new Error(errorMessage);
     }
-   let vid = document.querySelector('#source')
-   vid.setAttribute("src", data.url);
+    let vid = document.querySelector('#potdVideo');
+    vid.setAttribute("src", data.url);
+    
+    let title = document.querySelector('.potdTitle');
+    title.innerText = data.title; 
+
+    let date = document.querySelector('.potdDate');
+    date.innerText = data.date; 
+    
+    let description = document.querySelector('.potdDescription');
+    description.innerText = data.explanation;
   });
     
