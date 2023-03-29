0e440ecf42c5674f26d530a80ea2c0d63b1251d5
SierraRhodes <alreadywrote@gmail.com>
2023-03-29 14:16:41 -0700

    Add css and api calls

---
+++ src/space-phenomena/space-phenomena.js
@@ -0,0 +1,70 @@
+import spacePhenomena from '../api/space-phenomenaApiCall.js';
+import '../space-phenomena/css/space.css';
+
+
+const startDateInput = document.getElementById('start-date');
+const endDateInput = document.getElementById('end-date');
+
+const submitButton = document.getElementById('submit-button');
+
+submitButton.addEventListener('click', () => {
+  const startDate = startDateInput.value;
+  const endDate = endDateInput.value;
+
+  spacePhenomena.getSpace(startDate, endDate)
+    .then((data) => {
+      const resultsDiv = document.getElementById('results');
+      resultsDiv.innerHTML = '';
+
+      const selectedDate = new Date(startDate).toISOString().substring(0, 10);
+
+      data.forEach((item) => {
+        const flrDate = new Date(item.beginTime).toISOString().substring(0, 10); // Convert beginTime to ISO string format
+
+        if (flrDate === selectedDate) {
+          const newDiv = document.createElement('div');
+          newDiv.innerHTML = `<p>Start Time: ${item.beginTime}</p>
+                              <p>Peak Time: ${item.peakTime}</p>
+                              <p>End Time: ${item.endTime}</p>
+                              <p>Intensity: ${item.classType}</p>
+                              <p>Link: <a href="${item.link}" target="_blank">${item.link}</a></p>`;
+          resultsDiv.appendChild(newDiv);
+        }
+      });
+    })
+    .catch((error) => {
+      console.error(error);
+    });
+});
+
+/* global YT */
+
+function onYouTubeIframeAPIReady() {
+  new YT.Player('video-background', {
+    videoId: 'Sv3eXRN7hLo',
+    width: window.innerWidth,
+    height: window.innerHeight,
+    playerVars: {
+      autoplay: 1,
+      controls: 0,
+      loop: 1,
+      showinfo: 0,
+      modestbranding: 1
+    },
+    events: {
+      onReady: onPlayerReady
+    }
+  });
+}
+
+function onPlayerReady(event) {
+  event.target.mute();
+}
+
+window.onYouTubeIframeAPIReady = onYouTubeIframeAPIReady;
+
+
+
+
+
+
