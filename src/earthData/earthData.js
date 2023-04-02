079f13f2bcc290ae36f27fde390d64ffbc9649f6
Billy Lee <codingbillylee@gmail.com>
2023-04-02 10:26:45 -0700

    Add display earth image API call and functionality

--- src/earthData/earthData.js
+++ src/earthData/earthData.js
@@ -1,11 +1,25 @@
-// import earthPictureData from "../api/earthPicApiCall";
+import earthPictureData from "../api/earthPicApiCall";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
 let handleEarthPic = (event) => {
   event.preventDefault();
   let year = document.querySelector("#year").value;
-  console.log(year);
+  let month = document.querySelector("#month").value;
+  let day = document.querySelector("#day").value;
+
+  earthPictureData.getEarthPictureData(year, month, day)
+    .then((data) => {
+      if(data.result === "error") {
+        const errorMessage = `An error occured while fetching the currency data: ${data["error-type"]}`;
+        const errorElement = document.getElementById("error");
+        errorElement.innerHTML = errorMessage;
+        throw new Error(errorMessage);
+      }
+      
+      let img = document.querySelector('.slideshow-image');
+      img.setAttribute("src", `https://api.nasa.gov/EPIC/archive/natural/${year}/${month}/${day}/png/${data[0].image}.png?api_key=${process.env.API_KEY}`);
+    });
 };
 
 window.addEventListener("load", () => { 
