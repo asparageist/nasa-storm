c73df75b4903e11be0f521bec952b7a29e0694ea
Billy Lee <codingbillylee@gmail.com>
2023-04-04 21:22:26 -0700

    Add function to display the top three fastest astroids. Create function to add 0 onto any day that is less than 10

--- src/api/earthAstroidApiCall.js
+++ src/api/earthAstroidApiCall.js
@@ -1,7 +1,7 @@
 export default class astroidData {
   static async getAstroidData(year, month, day) {
     try {
-      const response = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${year}-${month}-${day}&end_date=2015-09-08&api_key=${process.env.API_KEY}`);
+      const response = await fetch(`https://api.nasa.gov/neo/rest/v1/feed?start_date=${year}-${month}-${day}&end_date=${year}-${month}-${parseInt(day) + 1}&api_key=${process.env.API_KEY}`);
       const data = await response.json();
 
       if (data.result === "error") {
--- src/earthData/earthData.js
+++ src/earthData/earthData.js
@@ -1,9 +1,22 @@
 import earthPictureData from "../api/earthPicApiCall";
+import astroidData from "../api/earthAstroidApiCall";
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './earthData.css';
 
+
 let photoIndex = 0;
+let otherDay = 0;
+
+let changeIndex = () => {
+  photoIndex++;
+  gatherInfo();
+};
+
+let prevIndex = () => {
+  photoIndex--;
+  gatherInfo();
+};
 
 let handleEarthPic = (event) => {
   event.preventDefault();
@@ -24,16 +37,66 @@ let gatherInfo = () => {
     year = 20 + year;
   }
 
-  if (month < 10) {
+  if (month.length < 2) {
     month = "0" + month;
   }
 
-  if (day < 10) {
+  if (day.length < 2) {
     day = "0" + day;
   }
-  changePic(year, month, day);
 
+  otherDay = parseInt(day);
+
+  changePic(year, month, day);
+  getAstroids(year, month, otherDay, day);
 };
+
+let getAstroids = (year, month, otherDay) => {
+  astroidData.getAstroidData(year, month, otherDay)
+    .then((data) => {
+      if(data.result === "error") {
+        const errorMessage = `An error occured while fetching the currency data: ${data["error-type"]}`;
+        const errorElement = document.getElementById("error");
+        errorElement.innerHTML = errorMessage;
+        throw new Error(errorMessage);
+      }
+      let addOneDay = otherDay + 1;
+      let totalAstroids = [];
+      let fastestAstroid = [];
+
+      if (otherDay < 10) {
+        otherDay = "0" + otherDay.toString();
+      }
+
+      if (addOneDay < 10) {
+        addOneDay = "0" + addOneDay;
+      }
+
+      let date_n1 = `${year}-${month}-${(addOneDay)}`;
+
+      let date = `${year}-${month}-${otherDay}`;
+
+      for (let i = 0; i < data.near_earth_objects[date].length; i++) {
+        totalAstroids.push(data.near_earth_objects[date][i].close_approach_data[0].relative_velocity.miles_per_hour);
+      }
+      
+      for (let i = 0; i < data.near_earth_objects[date_n1].length; i++) {
+        totalAstroids.push(data.near_earth_objects[date_n1][i].close_approach_data[0].relative_velocity.miles_per_hour);
+
+      }
+
+      totalAstroids.sort((a,b) => b-a);
+
+      fastestAstroid = totalAstroids.splice(0, 3);
+
+      document.querySelector(".numOfAstroids").innerText = data.element_count + ` Astroids flew by from ${date} to ${date_n1}`;
+      document.querySelector(".top1Astroids").innerText = `The fastest astroid was ${fastestAstroid[0]} mph`;
+      document.querySelector(".top2Astroids").innerText = `The second fastest astroid was ${fastestAstroid[1]} mph`;
+      document.querySelector(".top3Astroids").innerText = `The third fastest astroid was ${fastestAstroid[2]} mph`;
+    });
+};
+
+
 
 let changePic = (year, month, day) => {
   earthPictureData.getEarthPictureData(year, month, day)
@@ -43,7 +106,6 @@ let changePic = (year, month, day) => {
         const errorElement = document.getElementById("error");
         errorElement.innerHTML = errorMessage;
         throw new Error(errorMessage);
-
       }
 
       if (photoIndex >= data.length) {
@@ -67,16 +129,6 @@ let changePic = (year, month, day) => {
       let picNum = document.querySelector('.picNum');
       picNum.innerText = `${photoIndex + 1} / ${data.length}`;
     });
-};
-
-let changeIndex = () => {
-  photoIndex++;
-  gatherInfo();
-};
-
-let prevIndex = () => {
-  photoIndex--;
-  gatherInfo();
 };
 
 window.addEventListener("load", () => { 
